5087efcaeeb0e34963c49a6240d06f265ef559d1
Piloko1005 <leopiloco@gmail.com>
2024-08-26 20:34:33 -0500

    prueba primera version

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editorial</title>
+    <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="css/mi-estilo.css">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    
+    <style>
+        body{
+            background-image: url('img/name.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+        @import url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="title">
+            <h1>EDITORIAL</h1>
+            <p>PAGINAS INFINITAS</p>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="container-box">
+                    <h2>CUENTOS</h2>
+                    <div class="d-flex flex-wrap justify-content-center">
+                        <button class="btn btn-primary btn-circle btncolor-bluedark kanit-thin">La ultima generacion</button>
+                        <button class="btn btn-secondary btn-circle kanit-thin">2</button>
+                        <button class="btn btn-success btn-circle kanit-thin">3</button>
+                        <button class="btn btn-danger btn-circle kanit-thin">4</button>
+                        <button class="btn btn-warning btn-circle kanit-thin">5</button>
+                        <button class="btn btn-info btn-circle kanit-thin">6</button>
+                        <img src="img/name2.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <p>sdfsdfsdfsdfsdfsdffdfsdfffgfeffggffdgfrrrghrefgrefgf</p>
+                <img src="img/bot-a.jpeg" alt="">
+            </div>
+
+            <div class="col-md-6">
+                <div class="container-box flex-column-center">
+                    <h2>SOBRE NOSOTROS:</h2>
+                    <p>Texto descriptivo 1</p>
+                    <p>Texto descriptivo 2</p>
+                    <p>Texto descriptivo 3</p>
+                    <p>Texto descriptivo 4</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
