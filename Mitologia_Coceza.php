f2bece1965c38616e6d3a1d43e278acb072dbd86
JavaMaRIO-13 <mariohra2235@gmail.com>
2024-09-02 20:37:03 -0500

    Update MITOLOGIA COCEZA
    No c

--- Mitologia_Coceza.php
+++ Mitologia_Coceza.php
@@ -6,6 +6,21 @@
     <title>Mitologia Coceza</title>
 </head>
 <body>
-    
-</body>
+<div>
+        <h1>Botman</h1>
+    </div>
+    <div>
+        <p>En un tiempo indefinido dentro de un lapso infinito de tiempo, un titan llamado Urell vivia en un lugar tan alto, que sobrepasaba el Cosmos, mientras su hermano Deivar, era un titan que dormia bajo un oceano oscuro y caotico, no habia nada, y se llamaba "Rogteh". Urell sobrevolaba los cielos del Cosmos con su nube voladora y daba un paseo por todo este siempre. Un dia, Urell se sentia solo, pues no habia nadie quien lo acompañe, pues lo que hizo fue crear un universo gigantesco y prospero, donde iba a crear varias masas de tierra, roca y gas para formar lugares donde la vida pueda prosperar, y eso es lo que iba a pasar. Urell creo la vida en un planeta, que tenia condiciones hospitas para vivir, pero de la nada, emergio un titan creado por el polvo que le advirtio de que el y toda su familia iban a morir y que iba a destruir lo que mas aprecia. Urell, con cara de rechazo, ignoro al titan que se desvanecio. Despues, Urell hundio sus dedos en el monton de polvo que yacia en el planeta, y se crearon la tierra, luego lloro de tanta felicidad que creo charcos gigantescos en la interseccion de las tierras, creando los mares, rios y lagos, despues mando una carga de energia sobre todo lo creado, y se creo la vida. Plantas, animales, monstruos y de todo. Urell se sentia orgulloso de su creacion, pero luego sintio de que estas criaturas eran incapaces de progresar en la sociedad, por lo que creo a una especie inteligente, un humano, de hecho, humanos, ya que creo un hombre y una mujer, para que asi la descendencia se multiplique. En ese momento, el titan hizo su segunda aparicion, pero antes de que pueda decir una palabra, Urell lo mato, estaba harto de lo que le decia, pues le daba mucha intriga. Con el cuerpo del titan fue al centro del universo, ya que yacia apagado y oscuro, por lo tanto, creo con el cuerpo del titan, un brillante mundo, con una estructura ferrea hecha de las piedras mas preciosas, y decidio que ahi el haria una descendencia de dioses, para proteger a la humanidad. Llamo a ese mundo, Midharv.</p>
+        <img style="width:500px; height:500px;" src="img/bot-img_1.png" alt="">
+    </div>
+    <div>
+        <p>Luego una gran descendencia amplio, junto a dioses como Edzeon, el dios del sol y la verdad, Weatk, la diosa del mar y la prosperidad, Qhen, el dios del caos y la desesperacion, Fexendj, el dios del inframundo y los muertos, Trea, diosa de la luna y de la amistad y Uja, diosa del amor y la belleza. Ademas, hay dioses que tambien intervienen como Hion, dios de la estrategia y la guerra y Rehvalek, dios de la sabiduria y pensamiento. En adicion, tambien hay semidioses que aportaron mucho como Jytron, que era un semidios que protegia a la humanidad de criaturas monstruosas, matandolas y encerrandolas en una mazmorra llamada "Extraik", donde todos estos monstruos eran encerrados. Tambien habia una semidiosa llamada Cetaf, que cuidaba Midharv y observaba que todo vaya bien en el Cosmos. Los dioses tuvieron miles de descendientes, pero una en espcifico marco la historia de todo esto. Era Juwa, la diosa de la magia y el arte, era una diosa muy amable, pero ciertas veces rencorosa. Era hija de Qhen y Uja, y era a la que menos la querian, pues no era muy util para ellos.</p>
+        <img style="width:500px; height:500px;" src="img/bot-img_2.png" alt="">
+    </div>
+    <div>
+        <p></p>
+        <img style="width:500px; height:500px;" src="img/bot-img_3.png" alt="">
+    </div>
+    <footer>Derechos reservados, Mario Hernandez, 2024</footer>
+    </body>
 </html>
