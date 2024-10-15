8e8a0de5e0230a5685f6c47b8d8754177314b5d5
JavaMaRIO-13 <mariohra2235@gmail.com>
2024-10-14 21:01:50 -0500

    NOC
    14/10/2024

--- El_Conejo_de_la_Corona_Dorada.php
+++ El_Conejo_de_la_Corona_Dorada.php
@@ -6,6 +6,17 @@
     <title>El conejo de la corona dorada</title>
 </head>
 <body>
-    
+    <div>
+        <p>En una gran selva, un pequeño conejo daba brinquitos por esta. El veia animales mas grandes que el, y eso le aterrorizaba. Desde que era un pequeño conejito, soño con ser rey de la selva, pero el nunca creyo que podia serlo, era muy pequeño y debil para lograr serlo.</p>
+        <img style="width:500px; height:500px;" src="img/Bg.png" alt="">
+    </div>
+    <div>
+        <p></p>
+        <img style="width:500px; height:500px;" src="img/bot-img_2.png" alt="">
+    </div>
+    <div>
+        <p></p>
+        <img style="width:500px; height:500px;" src="img/bot-img_3.png" alt="">
+    </div>
 </body>
 </html>
