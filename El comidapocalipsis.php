ad6511365e94003f929fc1dcfcddfc03aa744929
JavaMaRIO-13 <mariohra2235@gmail.com>
2024-11-05 21:48:08 -0500

    Trabajo de hoy
    Perseus

--- El comidapocalipsis.php
+++ El comidapocalipsis.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>El conejo de la corona dorada</title>
+    <title>El comidapocalipsis</title>
 </head>
 <body>
     <div>
-        <p>En una gran selva, un pequeño conejo daba brinquitos por esta. El veia animales mas grandes que el, y eso le aterrorizaba. Desde que era un pequeño conejito, soño con ser rey de la selva, pero el nunca creyo que podia serlo, era muy pequeño y debil para lograr serlo.</p>
+        <p></p>
         <img style="width:500px; height:500px;" src="img/Bg.png" alt="">
     </div>
     <div>
